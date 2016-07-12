06687bfd1a4110ca1a5be85f666a2d1c85142302
Alan <371725153@qq.com>
2016-07-12 15:25:34 +0800

    1

---
+++ hello-flask.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#antuor:Alan
+
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Hello World</h1>'
+@app.route('/user/<name>')
+def user(name):
+    return('<h1>Hellow,%s!</h1>'% name)
+if __name__ == "__main__":
+    app.run(debug=True)
